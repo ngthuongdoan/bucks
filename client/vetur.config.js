42f1d9c2300db7e99c447a8d74eb47a8eb6d4ed7
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-14 19:36:51 +0700

    feat: add project config

--- client/vetur.config.js
+++ client/vetur.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // Notice: It only affects the settings used by Vetur.
   settings: {
     "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": true,
+    "vetur.experimental.templateInterpolationService": false,
   },
   // **optional** default: `[{ root: './' }]`
   // support monorepos
@@ -20,7 +20,7 @@ module.exports = {
       // Where is `package.json` in the project?
       // We use it to determine the version of vue.
       // It is relative to root property.
-      package: "./client/package.json",
+      package: "./package.json",
       // **optional**
       // Where is TypeScript config file in the project?
       // It is relative to root property.
@@ -33,7 +33,7 @@ module.exports = {
       // If you set it, you can get completion by that components.
       // It is relative to root property.
       // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
-      globalComponents: ["./client/src/components/**/*.vue"],
+      globalComponents: ["./src/components/**/*.vue"],
     },
   ],
 };
