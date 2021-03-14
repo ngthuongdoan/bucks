36d4c4a9045cbc89e2adf916ac8c8c9768131df5
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-14 11:09:40 +0700

    style: sonar effect

--- client/src/components/mobile/ActionButton.vue
+++ client/src/components/mobile/ActionButton.vue
@@ -2,7 +2,7 @@
   <div
     class="absolute flex items-center justify-center top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-14 h-14 rounded-full bg-control"
   >
-    <div class="w-9 h-9 rounded-full bg-control-light"></div>
+    <div class="w-9 h-9 rounded-full bg-control-light sonar"></div>
   </div>
 </template>
 
@@ -11,4 +11,22 @@ export default {};
 </script>
 
 <style>
+.sonar {
+  animation: sonar-effect 1s ease-in-out 0.1s infinite;
+}
+
+@keyframes sonar-effect {
+  0% {
+    opacity: 0.3;
+  }
+  40% {
+    opacity: 0.5;
+    box-shadow: 0 0 0 5px #fff, 0 0 10px 10px #fff, 0 0 0 10px #fff;
+  }
+  100% {
+    box-shadow: 0 0 0 5px #fff, 0 0 10px 10px #fff, 0 0 0 10px #fff;
+    transform: scale(0.5);
+    opacity: 0;
+  }
+}
 </style>
