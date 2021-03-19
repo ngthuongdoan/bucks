2b42396a6ac684829cc43ba2ee1c67f98af3ff31
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-19 10:53:58 +0700

    feat: enable camera

--- client/src/views/index/transaction/index.vue
+++ client/src/views/index/transaction/index.vue
@@ -15,34 +15,48 @@
           step="1000"
           value="0"
           class="add-input text-4xl font-bold text-center"
+          v-model="transaction.value"
         />
         <p class="text-center w-full ml-3">or</p>
-        <img
-          class="ml-3 grayscale"
-          src="https://img.icons8.com/metro/26/000000/camera.png"
-          @click="recognize"
+        <label for="image">
+          <img
+            class="ml-3 grayscale"
+            src="https://img.icons8.com/metro/26/000000/camera.png"
+          />
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          ref="fileInput"
+          capture="environment"
+          @change="recognize"
+          class="hidden"
         />
       </div>
       <div class="grid grid-cols-add items-end gap-y-2">
         <label for="username" class="font-bold self-start">Note</label>
-        <textarea class="add-input" rows="5"></textarea>
+        <textarea
+          class="add-input"
+          rows="5"
+          v-model="transaction.detail"
+        ></textarea>
         <label for="username" class="font-bold">Wallet</label>
-        <select class="add-input" name="" id="">
+        <select class="add-input" v-model="transaction.wallet">
           <option value="">Sacombank</option>
           <option value="">Overview</option>
           <option value="">Saving</option>
         </select>
         <label for="username" class="font-bold">Category</label>
-        <select class="add-input" name="" id="">
+        <select class="add-input" v-model="transaction.category">
           <option value="">A</option>
           <option value="">B</option>
           <option value="">C</option>
           c
         </select>
         <label for="username" class="font-bold">Date</label>
-        <input type="date" class="add-input" />
+        <input type="date" class="add-input" v-model="transaction.date" />
       </div>
-      <img src="~@/assets/img/test.png" id="text-img" class="hidden" />
     </form>
   </div>
 </template>
@@ -57,16 +71,42 @@ const worker = createWorker({
   logger: (m) => console.log(m),
 });
 export default {
+  data() {
+    return {
+      transaction: {
+        value: 0,
+        time: new Date(),
+        category: "",
+        detail: "",
+        image: "",
+        wallet: "",
+      },
+    };
+  },
   methods: {
+    uploadImage() {
+      const image = this.$refs.fileInput.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = (evt) => {
+        let img = evt.target.result;
+        this.transaction.image = img;
+      };
+    },
     async recognize() {
-      const img = document.getElementById("text-img");
-
-      await worker.load();
-      await worker.loadLanguage("vie");
-      await worker.initialize("vie");
-      const result = await worker.recognize(img);
-      console.log(result.data.lines);
-      await worker.terminate();
+      this.uploadImage();
+      this.$helpers.loading();
+      try {
+        await worker.load();
+        await worker.loadLanguage("vie");
+        await worker.initialize("vie");
+        const result = await worker.recognize(this.transaction.image);
+        this.transaction.detail = result.data.text;
+        await worker.terminate();
+        this.$helpers.close();
+      } catch (err) {
+        this.$helpers.showError(err);
+      }
     },
   },
 };
