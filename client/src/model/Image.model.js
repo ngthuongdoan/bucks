9322c500a100a138b3b4f16c300b8a2f7570e8c1
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-21 10:10:42 +0700

    style: create cloudinary model

--- client/src/model/Image.model.js
+++ client/src/model/Image.model.js
@@ -4,7 +4,21 @@
  * @classdesc -Class for Cloudinary API Response
  */
 class Image {
-  constructor() {}
+  /**
+   *
+   * @param {string} public_id - Defined image param
+   * @param {Date} created_at
+   * @param {string} url - HTTP url
+   * @param {string} secure_url - HTTPS url
+   * @param {string} original_filename - File name on cloud
+   */
+  constructor(public_id, created_at, url, secure_url, original_filename) {
+    this.public_id = public_id;
+    this.created_at = created_at;
+    this.url = url;
+    this.secure_url = secure_url;
+    this.original_filename = original_filename;
+  }
 }
 
 export default Image;
