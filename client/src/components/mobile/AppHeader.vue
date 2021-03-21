3a24a92cd8898b4ed6469be134b79c23c6bf88c3
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-21 10:10:18 +0700

    style: change card icon

--- client/src/components/mobile/AppHeader.vue
+++ client/src/components/mobile/AppHeader.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="fixed">
-    <img src="~@/assets/img/top-panel.svg" class="w-screen" />
+    <img src="~@/assets/img/top-panel.svg" alt="" class="w-screen"/>
     <h1
-      class="absolute z-10 top-5 left-5 font-bold font-sans text-xl text-white"
+        class="absolute z-10 top-5 left-5 font-bold font-sans text-xl text-white"
     >
       bucks
     </h1>
     <img
-      src="~@/assets/img/menu.svg"
-      alt=""
-      class="absolute z-10 top-7 right-5"
+        src="https://img.icons8.com/ios/64/000000/card-exchange.png"
+        alt=""
+        class="absolute z-10 top-7 right-5 w-6 cursor-pointer"
     />
     <wallet-card
-      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-20"
+        class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-20"
     ></wallet-card>
   </div>
 </template>
