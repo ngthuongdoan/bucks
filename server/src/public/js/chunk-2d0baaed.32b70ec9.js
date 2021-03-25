5e830ca28f3658a4a9ae3dc948ee042e991dc089
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-25 13:33:06 +0700

    feat: delete node serve

--- server/src/public/js/chunk-2d0baaed.32b70ec9.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0baaed"],{"37f9":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},a=[],c={},l=c,r=t("2877"),s=Object(r["a"])(l,u,a,!1,null,null,null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0baaed.32b70ec9.js.map
--- server/src/routes/v1/vue.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', () => {
-  res.sendFile(path + 'all.html');
-});
-
-module.exports = router;
