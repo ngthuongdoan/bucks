14ce6ab7c363c4d6a3108774161dfbb759060e4f
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-25 12:29:39 +0700

    ui: create wallet form

--- client/src/views/index/wallet/add/index.vue
+++ client/src/views/index/wallet/add/index.vue
@@ -1,7 +1,39 @@
 <template>
   <add-layout title="Add Wallet" @save="save">
-    <form>
-
+    <form class="flex flex-col m-auto gap-3 px-7 py-5">
+      <div class="grid grid-cols-add items-end gap-y-2">
+        <label class="font-bold self-start" for="name">Name</label>
+        <input
+            id="name"
+            v-model="wallet.name"
+            class="add-input"
+        />
+        <label class="font-bold" for="currency">Currency</label>
+        <select id="currency" v-model="wallet.currency" class="add-input">
+          <option selected value="">VND</option>
+          <option value="">USD</option>
+          <option value="">JPN</option>
+        </select>
+        <label class="font-bold" for="type">Type</label>
+        <select id="type" v-model="wallet.type" class="add-input">
+          <option selected value=""></option>
+          <option value="">Visa</option>
+          <option value="">USD</option>
+          <option value="">JPN</option>
+        </select>
+        <label class="font-bold self-start" for="initial">Initial</label>
+        <input
+            id="initial"
+            v-model="wallet.amount"
+            class="add-input"
+        />
+        <label class="font-bold self-start" for="serial">Serial</label>
+        <input
+            id="serial"
+            v-model="wallet.serial"
+            class="add-input"
+        />
+      </div>
     </form>
   </add-layout>
 </template>
