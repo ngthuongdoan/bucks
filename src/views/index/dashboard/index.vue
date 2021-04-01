81d7239b1835f3a119814d58e0463aba62fdeca7
ngthuongdoan <ngthuongdoan@gmail.com>
2021-04-01 20:10:06 +0700

    feat: create Transaction ui

--- src/views/index/dashboard/index.vue
+++ src/views/index/dashboard/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div v-if="isMobile">
     <app-header></app-header>
+    <div class="px-4 pt-80">
+      <div v-for="i in 50" :key="i" class=" px-3 py-2 my-3 flex w-full transaction items-center">
+        <img alt="" class="object-cover w-8 justify-self-start" src="~@/assets/icon/family.svg">
+        <h1 class="ml-3 font-bold text-sm">Khám bệnh</h1>
+        <span class="">120.000</span>
+      </div>
+    </div>
   </div>
   <div v-else>Desktop</div>
 </template>
 
 <script>
 import AppHeader from "@/components/mobile/AppHeader";
-import { isMobile } from "mobile-device-detect";
+import {isMobile} from "mobile-device-detect";
+
 export default {
   data() {
     return {
@@ -19,6 +27,3 @@ export default {
   },
 };
 </script>
-
-<style>
-</style>
