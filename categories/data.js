0d56b1342f94bd7af615c3e2dd95ca7eb1d9f6a9
ngthuongdoan <ngthuongdoan@gmail.com>
2021-04-05 17:21:47 +0700

    feat: add base income category

---
+++ categories/data.js
@@ -0,0 +1,26 @@
+export default [
+	{
+		"name": "award",
+		"type": "income"
+	},
+	{
+		"name": "gifts",
+		"type": "income"
+	},
+	{
+		"name": "interest money",
+		"type": "income"
+	},
+	{
+		"name": "salary",
+		"type": "income"
+	},
+	{
+		"name": "selling",
+		"type": "income"
+	},
+	{
+		"name": "other",
+		"type": "income"
+	}
+];
