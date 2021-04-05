60b7edef3023e04f29ca7900b0a9eeb38abbcefc
ngthuongdoan <ngthuongdoan@gmail.com>
2021-04-05 17:56:20 +0700

    refactor: modularize tesseract.js

--- src/plugin/tesseract.js
+++ src/plugin/tesseract.js
@@ -1,24 +1,17 @@
-import { createWorker, OEM, PSM } from "tesseract.js";
+import { createWorker } from "tesseract.js";
 
-let worker;
-export const loading = async () => {
-	worker = createWorker({
-		logger: (m) => console.log(m)
-	});
+const worker = createWorker({
+	// workerPath: 'https://unpkg.com/tesseract.js@v2.0.0/dist/worker.min.js',
+	langPath:
+		"https://raw.githubusercontent.com/naptha/tessdata/gh-pages/4.0.0_best/",
+	// corePath: 'https://unpkg.com/tesseract.js-core@v2.0.0/tesseract-core.wasm.js',
+	logger: (m) => console.log(m)
+});
 
+(async () => {
 	await worker.load();
-	await worker.loadLanguage("eng");
-	await worker.initialize("eng", OEM.LSTM_ONLY);
-	await worker.setParameters({
-		tessedit_pageseg_mode: PSM.SINGLE_BLOCK
-	});
+	await worker.loadLanguage("vie");
+	await worker.initialize("vie");
+})();
 
-	return worker;
-};
-
-export const recognize = async (img) => {
-	const {
-		data: { text }
-	} = await worker.recognize(img);
-	return text;
-};
+export default worker;
