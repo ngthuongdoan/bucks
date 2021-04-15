7a32e0cbd0787dbfa704f41b904707c1ead0f319
ngthuongdoan <ngthuongdoan@gmail.com>
2021-04-15 13:24:55 +0700

    feat (desktop dashboard): filter transactions

--- src/components/layout/DesktopDashboard.vue
+++ src/components/layout/DesktopDashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="desktop-dashboard">
+  <div v-if="wallet" class="desktop-dashboard">
     <wallet-card
         :wallet="wallet"
         class="relative"
@@ -11,7 +11,7 @@
         <button :class="['btn', activeOverview==='month'&&'btn-active']" @click="activeOverview='month'">Month</button>
         <button :class="['btn', activeOverview==='year'&&'btn-active']" @click="activeOverview='year'">Year</button>
       </div>
-      <div class="grid grid-cols-2 grid-rows-3 gap-2 w-max mt-3">
+      <div class="grid grid-cols-2 grid-rows-2 gap-2 w-max mt-3">
         <h1 class="font-bold text-lg">INCOME </h1><span
           class="ml-3 font-bold text-lg text-green-400">{{ 3000000 | separateValue }}</span>
         <h1 class="font-bold text-lg">EXPENSE </h1><span
@@ -28,19 +28,28 @@
       <div class="bg-white w-full h-full rounded-xl">
         <datepicker v-model="selectedDate"
                     format="dd/MM/yyyy"
-                    input-class=" top-2 left-3 text-gray-400 font-bold absolute cursor-pointer w-fit"
+                    input-class="top-2 left-3 text-gray-400 font-bold absolute cursor-pointer w-fit"
                     placeholder="Select Date"
-                    @selected="fetchTransaction"
         ></datepicker>
         <div class="py-10">
-          <app-transaction v-for="trans in transactions" :key="trans.id" :transaction="trans" class="shadow-none"/>
+          <div v-if="filterTransactions.length===0" class="text-center  italic text-gray-500">No Transactions</div>
+          <app-transaction v-for="trans in filterTransactions" v-else :key="trans.id" :transaction="trans"
+                           class="shadow-none"/>
         </div>
       </div>
     </div>
     <!--    Debt/Loan-->
-    <div class="w-full h-full">
+    <div class="w-full h-full  row-span-2">
       <h1 class="font-bold text-gray-700 text-lg">Debt/Loan</h1>
       <div class="bg-white w-full h-full rounded-xl">
+        <ul class="flex justify-around text-gray-400 font-bold cursor-pointer">
+          <li :class="['uppercase py-2 w-full h-full text-center rounded-tl-xl', isDebt?'text-black bg-white':'text-gray-500 bg-gray-200']">
+            Debt
+          </li>
+          <li :class="['uppercase py-2 w-full h-full  text-center rounded-tr-xl', !isDebt?'text-black bg-white':'text-gray-500 bg-gray-200']">
+            Loan
+          </li>
+        </ul>
         <div class="py-10">
         </div>
       </div>
@@ -56,21 +65,41 @@ import {transactionStore, walletStore} from "@/plugin/db";
 import store from "@/store"
 import WalletCard from "@/components/ui/WalletCard";
 import Datepicker from "vuejs-datepicker";
-import * as dayjs from "dayjs";
+import * as dayjs from "dayjs"
 
 export default {
   data() {
     return {
+      isDebt: true,
       transactions: [],
       wallet: {},
       activeOverview: "week",
-      selectedDate: new Date()
+      selectedDate: new Date(),
+      filterTransactions: [],
     };
   },
-  methods: {
-    fetchTransaction(evt) {
-      console.log(dayjs(evt).format("DD/MM/YYYY"))
+  watch: {
+    selectedDate: {
+      immediate: true,
+      handler: function (value) {
+        console.log(value)
+        this.filterTransactions = this.transactions.filter(trans => {
+          return dayjs(trans.time.toDate()).isSame(dayjs(value), "day");
+        })
+      }
     }
+  },
+  methods: {
+    // async fetchTransaction(time) {
+    //   const uid = store.getters["userModule/user"].data.uid;
+    //   const wallet = store.getters["userModule/user"].data.selectedWallet.id;
+    //   const transactionRefs = transactionStore
+    //       .where("uid", "==", uid)
+    //       .where("wallet.id", "==", wallet)
+    //       .where("time", "==", Timestamp(time));
+    //   const snapshot = await transactionRefs.get();
+    //   this.transactions = snapshot.data()
+    // }
   },
   components: {
     AppTransaction,
@@ -81,10 +110,10 @@ export default {
     const uid = store.getters["userModule/user"].data.uid;
     const wallet = store.getters["userModule/user"].data.selectedWallet.id;
     return {
-      wallet: walletStore.doc(wallet),
       transactions: transactionStore
           .where("uid", "==", uid)
-          .where("wallet.id", "==", wallet)
+          .where("wallet.id", "==", wallet),
+      wallet: walletStore.doc(wallet),
     };
   }
 };
