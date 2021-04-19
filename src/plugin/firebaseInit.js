1fb7da357ef5e587ed30db7fe15c409fb0908d71
ngthuongdoan <ngthuongdoan@gmail.com>
2021-04-19 20:45:46 +0700

    feat: authentication

--- src/plugin/firebaseInit.js
+++ src/plugin/firebaseInit.js
@@ -1,4 +1,13 @@
-import { firebaseConfig } from "@/config/firebase.config";
+import {firebaseConfig} from "@/config/firebase.config";
 import firebase from "firebase/app";
 
 firebase.initializeApp(firebaseConfig);
+
+firebase.getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+            unsubscribe();
+            resolve(user);
+        }, reject);
+    })
+};
--- src/views/index/dashboard/index.vue
+++ src/views/index/dashboard/index.vue
@@ -1,18 +1,26 @@
 <template>
-  <mobile-dashboard v-if="isMobile"></mobile-dashboard>
-  <desktop-dashboard v-else></desktop-dashboard>
+  <keep-alive v-if="user">
+    <mobile-dashboard v-if="isMobile"></mobile-dashboard>
+    <desktop-dashboard v-else></desktop-dashboard>
+  </keep-alive>
 </template>
 
 <script>
 import {isMobile} from "mobile-device-detect";
 import MobileDashboard from "@/components/layout/MobileDashboard";
 import DesktopDashboard from "@/components/layout/DesktopDashboard";
+import {mapGetters} from "vuex";
 
 export default {
   data() {
     return {
       isMobile,
     };
+  },
+  computed: {
+    ...mapGetters({
+      user: "userModule/user"
+    })
   },
   components: {
     MobileDashboard,
