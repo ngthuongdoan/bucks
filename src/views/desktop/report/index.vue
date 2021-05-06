5c87e6ca6e84c8160dd4ab8479c7bf32007b20b8
ngthuongdoan <ngthuongdoan@gmail.com>
2021-05-06 21:27:12 +0700

    feat: install echart

--- src/views/desktop/report/index.vue
+++ src/views/desktop/report/index.vue
@@ -1,10 +1,82 @@
 <template>
-  <div>REPORT</div>
+  <v-chart :option="option" class="chart"/>
 </template>
 
 <script>
-export default {};
+import {use} from "echarts/core";
+import {CanvasRenderer} from "echarts/renderers";
+import {PieChart} from "echarts/charts";
+import {LegendComponent, TitleComponent, TooltipComponent} from "echarts/components";
+import VChart, {THEME_KEY} from "vue-echarts";
+
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+]);
+
+export default {
+  name: "HelloWorld",
+  components: {
+    VChart
+  },
+  provide: {
+    [THEME_KEY]: "dark"
+  },
+  data() {
+    return {
+      option: {
+        title: {
+          text: "Traffic Sources",
+          left: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: [
+            "Direct",
+            "Email",
+            "Ad Networks",
+            "Video Ads",
+            "Search Engines"
+          ]
+        },
+        series: [
+          {
+            name: "Traffic Sources",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              {value: 335, name: "Direct"},
+              {value: 310, name: "Email"},
+              {value: 234, name: "Ad Networks"},
+              {value: 135, name: "Video Ads"},
+              {value: 1548, name: "Search Engines"}
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.chart {
+  height: 400px;
+}
 </style>
--- src/views/mobile/report/index.vue
+++ src/views/mobile/report/index.vue
@@ -1,10 +1,3 @@
 <template>
   <div>REPORT</div>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style>
-</style>
