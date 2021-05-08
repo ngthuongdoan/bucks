7f82f3a076c3029f439faad03bbc4e4b338dafa6
ngthuongdoan <ngthuongdoan@gmail.com>
2021-05-08 12:30:55 +0700

    feat(Desktop): get trans by day

---
+++ src/components/chart/BarChart.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-chart v-if="option" :option="option" class="w-full h-96"/>
+</template>
+
+<script>
+import {use} from "echarts/core";
+import {CanvasRenderer} from "echarts/renderers";
+import {BarChart} from "echarts/charts";
+import {GridComponent, LegendComponent, TitleComponent, TooltipComponent} from "echarts/components";
+import VChart, {THEME_KEY} from "vue-echarts";
+import {mapGetters} from "vuex";
+
+use([
+  CanvasRenderer,
+  BarChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+]);
+
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+    legends: {
+      type: Array,
+      required: true,
+    },
+    xAxis: {
+      type: Object
+    },
+    above: {
+      type: Object
+    },
+    below: {
+      type: Object
+    },
+  },
+  components: {
+    VChart
+  },
+  provide: {
+    [THEME_KEY]: "light"
+  },
+  computed: {
+    ...mapGetters({
+      user: "userModule/user"
+    }),
+    option() {
+      const emphasisStyle = {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowColor: 'rgba(0,0,0,0.3)'
+        }
+      };
+      const title = this.title;
+      const legends = this.legends;
+      const xAxis = this.xAxis;
+      const above = this.above;
+      const below = this.below;
+
+      return {
+        autoresize: true,
+        title: {
+          text: title,
+          left: "center"
+        },
+        tooltip: {},
+        legend: {
+          data: [...legends],
+          left: '10%'
+        },
+        xAxis: {
+          data: xAxis.data,
+          name: xAxis.name,
+          axisLine: {onZero: true},
+          splitLine: {show: false},
+          splitArea: {show: false}
+        },
+        yAxis: {},
+        grid: {
+          bottom: 100
+        },
+        series: [
+          {
+            name: above.name,
+            type: 'bar',
+            stack: 'one',
+            emphasis: emphasisStyle,
+            data: above.data,
+            itemStyle: {
+              color: "rgb(83,151,83)"
+            }
+          },
+          {
+            name: below.name,
+            type: 'bar',
+            stack: 'one',
+            emphasis: emphasisStyle,
+            data: below.data,
+            itemStyle: {
+              color: "rgb(243,130,95)"
+            }
+          },
+        ]
+      }
+    }
+  }
+  // created() {
+  //   setInterval(() => {
+  //     let data0 = this.option.series[0].data;
+  //     let data1 = this.option.series[1].data;
+  //     data0.shift();
+  //     data0.push((Math.random() * 2).toFixed(2));
+  //     data1.shift();
+  //     data1.push(-Math.random().toFixed(2));
+  //   }, 2100);
+  // },
+};
+</script>
+
---
+++ src/components/layout/ReportOverview.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="bg-white  w-full h-1/2 pt-5">
+    <div class="ml-10 flex gap-2 justify-start items-start">
+      <div class="flex flex-col">
+        <label class="text-sm text-gray-400">Months</label>
+        <select v-model="range">
+          <option
+              v-for="(m, index) in months"
+              :key="m"
+              :value="index"
+          >{{ m }}
+          </option>
+        </select>
+      </div>
+      <div class="flex flex-col">
+        <label class="text-sm text-gray-400" for="">Wallets</label>
+        <select v-model="walletId">
+          <option
+              v-for="wallet in wallets"
+              :key="wallet.id"
+              :value="wallet.id"
+          >{{ wallet.name }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <bar-chart
+        :above="above"
+        :below="below"
+        :legends="['Income', 'Expense']"
+        :x-axis="{name:'Day', data: dayOfMonth}"
+        title="Monthly Income-Expense"
+    ></bar-chart>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+import {transactionStore, walletStore} from "@/plugin/db";
+import BarChart from "@/components/chart/BarChart";
+import store from "@/store"
+import * as dayjs from "dayjs";
+import * as isLeapYear from 'dayjs/plugin/isLeapYear';
+
+dayjs.extend(isLeapYear)
+
+export default {
+  components: {
+    BarChart
+  },
+  data() {
+    let expense = [];
+    let income = [];
+
+    const months = Array.from({length: 12}, (item, i) => {
+      income.push((Math.random() * 2).toFixed(2));
+      expense.push(-Math.random().toFixed(2));
+      return new Date(0, i).toLocaleString('en-US', {month: 'long'})
+    });
+
+    return {
+      transactions: [],
+      range: dayjs().month(),
+      months,
+      walletId: "",
+      wallets: []
+    }
+  },
+  computed: {
+    ...mapGetters({
+      user: "userModule/user"
+    }),
+    // walletId:{
+    //   get: () => this.user.data.selectedWallet,
+    //   set: (value) => {this.walletId = value}
+    // },
+    dayOfMonth() {
+      const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if (dayjs().isLeapYear()) days[1] = 29;
+      return Array.from({length: days[this.range]}, (_, i) => i + 1)
+    },
+    above() {
+      const start = dayjs().month(this.range).startOf('month').toDate();
+      const end = dayjs().month(this.range).endOf('month').toDate();
+      let data = []
+      for (let d = start; d <= end; d.setDate(d.getDate() + 1)) {
+        const sum = this.transactions
+            .filter(trans =>
+                (trans.category.type === "income")
+                &&
+                (dayjs(trans.time.toDate())
+                        .isSame(dayjs(d), "day")
+                )
+            )
+            .reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
+        data.push(sum);
+      }
+      return {
+        name: 'Income',
+        data: data
+      }
+    },
+    below() {
+      const start = dayjs().month(this.range).startOf('month').toDate();
+      const end = dayjs().month(this.range).endOf('month').toDate();
+      let data = []
+      for (let d = start; d <= end; d.setDate(d.getDate() + 1)) {
+        const sum = this.transactions
+            .filter(trans =>
+                (trans.category.type === "expense")
+                &&
+                (dayjs(trans.time.toDate())
+                        .isSame(dayjs(d), "day")
+                )
+            )
+            .reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
+        data.push(sum);
+      }
+      return {
+        name: 'Expense',
+        data: data
+      }
+    }
+  },
+  watch: {
+    range: {
+      immediate: true,
+      handler: function () {
+        this.fetchTransaction()
+      }
+    },
+    walletId() {
+      this.fetchTransaction()
+    }
+  },
+  methods: {
+    fetchTransaction() {
+      this.$bind(
+          "transactions",
+          transactionStore
+              .where("uid", "==", this.user.data.uid)
+              .where("wallet", "==", this.walletId)
+              .where("time", "<=", dayjs().month(this.range).endOf('month').toDate())
+              .where("time", ">=", dayjs().month(this.range).startOf('month').toDate())
+      );
+    }
+  },
+  firestore() {
+    const uid = store.getters["userModule/user"].data.uid;
+    return {
+      wallets: walletStore.where("uid", "==", uid)
+    }
+  }
+};
+</script>
+
