0d1551b5447e449592643abd4d1377291a1c4425
ngthuongdoan <ngthuongdoan@gmail.com>
2021-05-10 11:34:20 +0700

    ui(Mobile): create ChangeView.vue
    Three buttons for change the view of statistic

--- src/components/layout/ReportOverview.vue
+++ src/components/layout/ReportOverview.vue
@@ -2,7 +2,7 @@
   <div class="bg-white  w-full h-1/2 pt-5">
     <div class="ml-10 flex gap-2 justify-start items-start">
       <div class="flex flex-col">
-        <label class="text-sm text-gray-400">Months</label>
+        <label class="text-sm text-gray-400 capitalize">{{ "Month" }}</label>
         <select v-model="range">
           <option
               v-for="(m, index) in months"
@@ -28,8 +28,8 @@
     <bar-chart
         :above="above"
         :below="below"
-        :legends="['Income', 'Expense']"
-        :x-axis="{name:'Day', data: dayOfMonth}"
+        :legends="['income', 'expense']"
+        :x-axis="xAxis"
         title="Monthly Income-Expense"
     ></bar-chart>
   </div>
@@ -50,96 +50,101 @@ export default {
     BarChart
   },
   data() {
-    let expense = [];
-    let income = [];
-
     const months = Array.from({length: 12}, (item, i) => {
-      income.push((Math.random() * 2).toFixed(2));
-      expense.push(-Math.random().toFixed(2));
       return new Date(0, i).toLocaleString('en-US', {month: 'long'})
     });
 
     return {
       transactions: [],
-      range: dayjs().month(),
       months,
       walletId: "",
-      wallets: []
+      wallets: [],
+      range: dayjs().month(),
     }
   },
   computed: {
     ...mapGetters({
       user: "userModule/user"
     }),
-    dayOfMonth() {
-      const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-      if (dayjs().isLeapYear()) days[1] = 29;
-      return Array.from({length: days[this.range]}, (_, i) => i + 1)
+    startRange() {
+      return dayjs().set("month", this.range).startOf("month").toDate();
+    },
+    endRange() {
+      return dayjs().set("month", this.range).endOf("month").toDate();
     },
     above() {
-      const start = dayjs().month(this.range).startOf('month').toDate();
-      const end = dayjs().month(this.range).endOf('month').toDate();
-      let data = []
-      for (let d = start; d <= end; d.setDate(d.getDate() + 1)) {
-        const sum = this.transactions
-            .filter(trans =>
-                (trans.category.type === "income")
-                &&
-                (dayjs(trans.time.toDate())
-                        .isSame(dayjs(d), "day")
-                )
-            )
-            .reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
-        data.push(sum);
-      }
-      return {
-        name: 'Income',
-        data: data
-      }
+      return this.createData("income")
     },
     below() {
-      const start = dayjs().month(this.range).startOf('month').toDate();
-      const end = dayjs().month(this.range).endOf('month').toDate();
-      let data = []
-      for (let d = start; d <= end; d.setDate(d.getDate() + 1)) {
-        const sum = this.transactions
-            .filter(trans =>
-                (trans.category.type === "expense")
-                &&
-                (dayjs(trans.time.toDate())
-                        .isSame(dayjs(d), "day")
-                )
-            )
-            .reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
-        data.push(sum);
-      }
-      return {
-        name: 'Expense',
-        data: data
-      }
+      return this.createData("expense")
+    },
+    xAxis() {
+      let data = [];
+      const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if (dayjs().isLeapYear()) days[1] = 29;
+      data = Array.from({length: days[this.range]}, (_, i) => i + 1)
+      // switch (this.activeOverview) {
+      //   case "week": {
+      //     const today = new Date();
+      //     const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+      //     const pastDaysOfYear = (today - firstDayOfYear) / 86400000;
+      //     const len = Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+      //     data = Array.from({length: len}, (_, i) => i + 1)
+      //     break;
+      //   }
+      //   case "month": {
+      //     const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      //     if (dayjs().isLeapYear()) days[1] = 29;
+      //     data = Array.from({length: days[this.range]}, (_, i) => i + 1)
+      //     break;
+      //   }
+      //   case "year": {
+      //     const min = 2000, max = dayjs().year();
+      //     for (let i = min; i <= max; i++) {
+      //       data.push(i)
+      //     }
+      //     break;
+      //   }
+      // }
+      return {name: "month", data}
     }
   },
   watch: {
-    range: {
-      immediate: true,
-      handler: function () {
-        this.fetchTransaction()
-      }
-    },
     walletId() {
       this.fetchTransaction()
     }
   },
   methods: {
-    fetchTransaction() {
-      this.$bind(
+    async fetchTransaction() {
+      await this.$bind(
           "transactions",
           transactionStore
               .where("uid", "==", this.user.data.uid)
               .where("wallet", "==", this.walletId)
-              .where("time", "<=", dayjs().month(this.range).endOf('month').toDate())
-              .where("time", ">=", dayjs().month(this.range).startOf('month').toDate())
+              .where("time", "<=", this.endRange)
+              .where("time", ">=", this.startRange)
       );
+    },
+    createData(type) {
+      let data = []
+      const start = dayjs().set("month", this.range).startOf("month").toDate();
+      const end = dayjs().set("month", this.range).endOf("month").toDate()
+      for (let d = start; d <= end; d.setDate(d.getDate() + 1)) {
+        const sum = this.transactions
+            .filter(trans =>
+                (trans.category.type === type)
+                &&
+                (dayjs(trans.time.toDate())
+                        .isSame(dayjs(d), "day")
+                )
+            )
+            .reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
+        data.push(sum);
+      }
+      return {
+        name: type,
+        data: data
+      }
     }
   },
   firestore() {
---
+++ src/components/ui/ChangeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex gap-3">
+    <button :class="['btn ', activeOverview==='week'&&'btn-active main-btn']" @click="changeView('week')">Week
+    </button>
+    <button :class="['btn ', activeOverview==='month'&&'btn-active main-btn']" @click="changeView('month')">Month
+    </button>
+    <button :class="['btn ', activeOverview==='year'&&'btn-active main-btn']" @click="changeView('year')">Year
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'change-view',
+  props: {
+    activeOverview: {
+      type: String,
+      validate(value) {
+        return ["week", "month", "year"].includes(value);
+      }
+    }
+  },
+  methods: {
+    changeView(view) {
+      this.activeOverview = view
+    }
+  }
+}
+</script>
