e9c8eab34c96cda78c2277c39a366a5a8df0086e
ngthuongdoan <ngthuongdoan@gmail.com>
2021-06-05 14:20:08 +0700

    ui: hide setting

--- src/components/modal/SettingModal.vue
+++ src/components/modal/SettingModal.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col justify-start items-start gap-3">
-    <div v-for="(s,index) in setting" :key="s.name" class="w-full flex gap-2">
-      <label class="flex-grow">{{ s.name }}</label>
-      <toggle-button :color="{checked: '#52734d', unchecked: '#d5ecca'}"
-                     :value="settingValue[index]"
-                     @click="settingValue[index] = !settingValue[index]"
-      />
-    </div>
+    <!--    <div v-for="(s,index) in setting" :key="s.name" class="w-full flex gap-2">-->
+    <!--      <label class="flex-grow">{{ s.name }}</label>-->
+    <!--      <toggle-button :color="{checked: '#52734d', unchecked: '#d5ecca'}"-->
+    <!--                     :value="settingValue[index]"-->
+    <!--                     @click="settingValue[index] = !settingValue[index]"-->
+    <!--      />-->
+    <!--    </div>-->
     <div class="w-full flex gap-2">
       <label class="flex-grow">{{ $t("modal.language") }}</label>
       <div
@@ -18,44 +18,42 @@
           @click="changeLanguage('en')"
       ></div>
     </div>
-    <div class="mt-3 w-full">
-      <button
-          class="main-btn p-3 w-full"
-          @click="reset"
-      > RESET
-      </button>
-    </div>
+    <!--    <div class="mt-3 w-full">-->
+    <!--      <button-->
+    <!--          class="main-btn p-3 w-full"-->
+    <!--          @click="reset"-->
+    <!--      > RESET-->
+    <!--      </button>-->
+    <!--    </div>-->
   </div>
 </template>
 
 <script>
-import {ToggleButton} from 'vue-js-toggle-button';
-import setting from "@/config/settings.json"
 
 export default {
   name: "SettingModal",
-  data() {
-    return {
-      setting,
-      defaultSetting: [false, true, false],
-      settingValue: [false, true, false],
-    }
-  },
+  // data() {
+  //   return {
+  //     setting,
+  //     defaultSetting: [false, true, false],
+  //     settingValue: [false, true, false],
+  //   }
+  // },
   computed: {
     lang() {
       return this.$i18n.locale;
     }
   },
-  components: {
-    ToggleButton,
-  },
+  // components: {
+  //   ToggleButton,
+  // },
   methods: {
     changeLanguage(lang) {
       this.$i18n.locale = lang
     },
-    reset() {
-      this.settingValue = [...this.defaultSetting]
-    }
+    // reset() {
+    //   this.settingValue = [...this.defaultSetting]
+    // }
   }
 }
 </script>
